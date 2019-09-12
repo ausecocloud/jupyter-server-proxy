145804e0e0d7599c2322599834851483d99d195e
Gerhard Weis <g.weis@griffith.edu.au>
2019-09-12 13:14:06 +1000

    extend request_timeout for proxy requests to avoid error messages for RStudio POST /events/get_events (times out after 50s)

--- jupyter_server_proxy/handlers.py
+++ jupyter_server_proxy/handlers.py
@@ -282,7 +282,10 @@ class ProxyHandler(WebSocketHandlerMixin, IPythonHandler):
     def proxy_request_options(self):
         '''A dictionary of options to be used when constructing
         a tornado.httpclient.HTTPRequest instance for the proxy request.'''
-        return dict(follow_redirects=False)
+        return dict(
+            follow_redirects=False,
+            request_timeout=60,
+        )
 
     def check_xsrf_cookie(self):
         '''
